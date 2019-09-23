402e79c367d3ffcc38252c5d0b449657622963c1
Henrique Reno Sawada <renohh@gmail.com>
2019-09-23 02:22:38 -0300

    [ex01] identacao

--- EX01/main/main.c
+++ EX01/main/main.c
@@ -2,7 +2,7 @@
  * Lib C
  */
 #include <stdio.h>
-#include <stdint.h>  
+#include <stdint.h>
 
 /*
  * FreeRTOS
@@ -29,9 +29,9 @@
 static const char * TAG = "MAIN: ";
 
 /*
- * Variável Global. 
+ * Variável Global.
  */
-QueueHandle_t xQueue; 
+QueueHandle_t xQueue;
 
 
 /*
@@ -41,18 +41,18 @@ QueueHandle_t xQueue;
 void task_adc( void * pvParameter )
 {
 	/* uint32_t é um typedef padrão definido em stdint.h */
-	uint32_t count = 0; 
+	uint32_t count = 0;
 	uint32_t status;
-	
+
 	/* As Tasks normalmente possuem um loop sem saída */
 	for(;;)
 	{
 		/*
 		 * Envia na fila o valor da variável count;
 		 * Caso a fila esteja cheia será retornado imediatamente;
-		 * Sendo assim, 
+		 * Sendo assim,
 		 */
-		
+
 		/* status_return = xQueueSend( fila_name, variável_address, timeout) */
 		status = xQueueSend(xQueue, &count, 0);
 		if(status == pdPASS)
@@ -63,12 +63,12 @@ void task_adc( void * pvParameter )
 		/* Bloqueia esta task por 1s */
 		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
-	
-	/* este comando não deveria ser executado... Caso algo estranho aconteça, 
+
+	/* este comando não deveria ser executado... Caso algo estranho aconteça,
 	 * esta task será deletada; O parametro NULL informa que é para desalocar
 	 * da memória ESTA task;
 	*/
-    vTaskDelete(NULL);	
+    vTaskDelete(NULL);
 }
 
 /*
@@ -77,19 +77,19 @@ void task_adc( void * pvParameter )
 */
 void task_print( void * pvParameter )
 {
-	uint32_t count; 
+	uint32_t count;
 	uint32_t status;
-	
+
 	for(;;)
 	{
 		/* status_return = xQueueReceive( fila_name, variável_address, timeout) */
-		status = xQueueReceive(xQueue, &count, 0); 
+		status = xQueueReceive(xQueue, &count, 0);
 		if(status == pdPASS)
-		{		
-			ESP_LOGI( TAG, "COUNT RECEBIDO. VALOR = %d\n", count );	
+		{
+			ESP_LOGI( TAG, "COUNT RECEBIDO. VALOR = %d\n", count );
 		}
-		
-		vTaskDelay(1000 / portTICK_PERIOD_MS);	
+
+		vTaskDelay(1000 / portTICK_PERIOD_MS);
 	}
     vTaskDelete(NULL);
 }
@@ -102,13 +102,13 @@ void blink_task(void *pvParameter)
 {
 	/*
 	 * Configura a GPIO2 do ESP32 como GPIO-OUTPUT;
-	 * Sobre o Led Building do ESP32; 
+	 * Sobre o Led Building do ESP32;
 	 * Ligado 		-> GPIO2 - Nível 0
 	 * Desligado 	-> GPIO2 - Nível 1
 	*/
     gpio_pad_select_gpio(BLINK_GPIO);
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
-	
+
     for(;;)
 	{
         /* Liga Led */
@@ -131,35 +131,30 @@ void app_main()
 		ESP_LOGI( TAG, "error - nao foi possivel alocar xQueue.\n" );
 		/* app_main será finalizada e o programa finalizado */
 		return;
-	} 
-	
+	}
+
 	/* configMINIMAL_STACK_SIZE é um #define que informa para o FreeRTOS qual o tamanho mínimo
-	necessário para rodar uma task. Por meio do menuconfig é possível saber o tamanho de configMINIMAL_STACK_SIZE; 
+	necessário para rodar uma task. Por meio do menuconfig é possível saber o tamanho de configMINIMAL_STACK_SIZE;
 	*/
     if( (xTaskCreate( blink_task, "blink_task", configMINIMAL_STACK_SIZE, NULL, 5, NULL) ) != pdTRUE )
 	{
-		ESP_LOGI( TAG, "error - nao foi possivel alocar blink_task.\n" );	
-		return;		
+		ESP_LOGI( TAG, "error - nao foi possivel alocar blink_task.\n" );
+		return;
 	}
-	
+
 	/*
 	 * Task_print e task_adc contém a função printf (ESP_LOGI). Portanto, foi aumentado o stack
 	 * para 2k;
 	 */
 	if( (xTaskCreate( task_print, "task_print", 4048, NULL, 5, NULL)) != pdTRUE )
 	{
-		ESP_LOGI( TAG, "error - nao foi possivel alocar task_print.\n" );	
-		return;		
+		ESP_LOGI( TAG, "error - nao foi possivel alocar task_print.\n" );
+		return;
 	}
 	if( (xTaskCreate( task_adc, "task_adc", 4048, NULL, 5, NULL)) != pdTRUE )
 	{
-		ESP_LOGI( TAG, "error - nao foi possivel alocar task_adc.\n" );	
-		return;		
+		ESP_LOGI( TAG, "error - nao foi possivel alocar task_adc.\n" );
+		return;
 	}
-	
+
 }
-
-
-
-
-
