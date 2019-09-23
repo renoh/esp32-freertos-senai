0c2d66254567df2c4ffd250b1183a8e2e6009daf
Henrique Reno Sawada <renohh@gmail.com>
2019-09-23 03:12:10 -0300

    [ex03] identacao

--- EX03/main/main.c
+++ EX03/main/main.c
@@ -2,7 +2,7 @@
  * Lib C
  */
 #include <stdio.h>
-#include <stdint.h>  
+#include <stdint.h>
 #include <string.h>
 
 /*
@@ -31,9 +31,9 @@
 static const char * TAG = "MAIN: ";
 
 /*
- * Variável Global. 
+ * Variável Global.
  */
-QueueHandle_t xQueue_button; 
+QueueHandle_t xQueue_button;
 
 
 /*
@@ -53,7 +53,7 @@ void task_button( void * pvParameter )
      */
     gpio_pad_select_gpio( BUTTON_GPIO );
     gpio_set_direction( BUTTON_GPIO, GPIO_MODE_INPUT );
-    gpio_set_pull_mode( BUTTON_GPIO, GPIO_PULLUP_ONLY );	
+    gpio_set_pull_mode( BUTTON_GPIO, GPIO_PULLUP_ONLY );
 
 	/* As Tasks normalmente possuem um loop sem saída */
 	for(;;)
@@ -68,7 +68,7 @@ void task_button( void * pvParameter )
 			 * Delay necessário para não processar o bounce causado
 			 * pelo acionamento do botão;
 			 */
-			vTaskDelay( 100 / portTICK_PERIOD_MS ); 
+			vTaskDelay( 100 / portTICK_PERIOD_MS );
 
 			/**
 			 * O botão ainda está sendo pressionado? Sim, então...
@@ -83,9 +83,9 @@ void task_button( void * pvParameter )
 				if( xQueueSend( xQueue_button, &txt, (10/portTICK_PERIOD_MS) ) == pdPASS )
 				{
 					ESP_LOGI( TAG, "\n\n Mensagem enviada com sucesso!" );
-					
+
 				}
-				status = 1; 				
+				status = 1;
 			}
 
 		}
@@ -99,7 +99,7 @@ void task_button( void * pvParameter )
 			 * Delay necessário para não processar o bounce causado
 			 * pelo acionamento do botão;
 			 */
-			vTaskDelay( 100 / portTICK_PERIOD_MS ); 
+			vTaskDelay( 100 / portTICK_PERIOD_MS );
 
 			/**
 			 * O botão ainda está realmente solto? Sim, então...
@@ -107,19 +107,19 @@ void task_button( void * pvParameter )
 			if( gpio_get_level(BUTTON_GPIO) == 1 && status == 1)
 			{
 				ESP_LOGI( TAG, "\n\n Button foi solto." );
-				status = 0; 
+				status = 0;
 			}
 		}
-		
+
 		/* Bloqueia esta task por 10ms */
 		vTaskDelay( 10/portTICK_PERIOD_MS );
 	}
-	
-	/* este comando não deveria ser executado... Caso algo estranho aconteça, 
+
+	/* este comando não deveria ser executado... Caso algo estranho aconteça,
 	 * esta task será deletada; O parametro NULL informa que é para desalocar
 	 * da memória ESTA task;
 	*/
-    vTaskDelete(NULL);	
+    vTaskDelete(NULL);
 }
 
 /*
@@ -128,17 +128,17 @@ void task_button( void * pvParameter )
 */
 void task_print( void * pvParameter )
 {
-	char * txt = NULL; 
-	
+	char * txt = NULL;
+
 	for(;;)
 	{
 		/**
 		 * Aguarda a chegada de algum valor na fila;
 		 */
-		xQueueReceive( xQueue_button, &txt, portMAX_DELAY ); 	
+		xQueueReceive( xQueue_button, &txt, portMAX_DELAY );
 		ESP_LOGI( TAG, "\n\n Mensagem Recebida = %s", txt );
-		
-		
+
+
 		/* Bloqueia intencionalmente esta task por 10ms */
 		vTaskDelay( 10/portTICK_PERIOD_MS );
 	}
@@ -156,7 +156,7 @@ void task_led(void *pvParameter)
 {
 	/*
 	 * Configura a GPIO2 do ESP32 como GPIO-OUTPUT;
-	 * Sobre o Led Building do ESP32; 
+	 * Sobre o Led Building do ESP32;
 	 * Ligado 		-> GPIO2 - Nível 0
 	 * Desligado 	-> GPIO2 - Nível 1
 	*/
@@ -185,35 +185,30 @@ void app_main()
 		ESP_LOGI( TAG, "error - nao foi possivel alocar xQueue_button.\n" );
 		/* app_main será finalizada e o programa finalizado */
 		return;
-	} 
-	
+	}
+
 	/* configMINIMAL_STACK_SIZE é um #define que informa para o FreeRTOS qual o tamanho mínimo
-	necessário para rodar uma task. Por meio do menuconfig é possível saber o tamanho de configMINIMAL_STACK_SIZE; 
+	necessário para rodar uma task. Por meio do menuconfig é possível saber o tamanho de configMINIMAL_STACK_SIZE;
 	*/
     if( ( xTaskCreate( task_led, "task_led", configMINIMAL_STACK_SIZE, NULL, 5, NULL )) != pdTRUE )
 	{
-		ESP_LOGI( TAG, "error - nao foi possivel alocar task_led.\n" );	
-		return;		
+		ESP_LOGI( TAG, "error - nao foi possivel alocar task_led.\n" );
+		return;
 	}
-	
+
 	/*
 	 * Task_print e task_adc contém a função printf (ESP_LOGI). Portanto, foi aumentado o stack
 	 * para 2k;
 	 */
 	if( (xTaskCreate( task_print, "task_print", 2048, NULL, 5, NULL )) != pdTRUE )
 	{
-		ESP_LOGI( TAG, "error - nao foi possivel alocar task_print.\n" );	
-		return;		
+		ESP_LOGI( TAG, "error - nao foi possivel alocar task_print.\n" );
+		return;
 	}
 	if( (xTaskCreate( task_button, "task_button", 2048, NULL, 5, NULL )) != pdTRUE )
 	{
-		ESP_LOGI( TAG, "error - nao foi possivel alocar task_button.\n" );	
-		return;		
+		ESP_LOGI( TAG, "error - nao foi possivel alocar task_button.\n" );
+		return;
 	}
-	
+
 }
-
-
-
-
-
